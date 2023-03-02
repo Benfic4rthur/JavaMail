e427faa8c42b0becaa4e8b2267053ef965367038
Arthur Benfica <Arthur Benfica@PC-ARTHUR>
2023-03-02 12:37:31 -0300

    Classe configurada para ter um pouco mais de dinamismo.

--- src/test/java/enviando/email/AppTest.java
+++ src/test/java/enviando/email/AppTest.java
@@ -3,6 +3,9 @@ package enviando.email;
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class AppTest {
 	public static void main(String[] args) {
@@ -22,28 +25,41 @@ public class AppTest {
 		props.put("mail.smtp.socketfactory.port", "465");
 		props.put("mail.smtp.socketfactory.class", "javax.net.ssl.SSLSocketFactory");
 
-		// Criação da sessão
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+		
+		int enviar = JOptionPane.showConfirmDialog(null, "Deseja enviar um email?");
+		
+		if(enviar == 0) {
+			String enderecoEmail = JOptionPane.showInputDialog("Digite o e-mail para qual quer fazer o envio:");
+			String assuntoEmail = JOptionPane.showInputDialog("Digite o Assunto do e-mail:");
+			JTextArea mensagemEmail = new JTextArea(10, 50);
+			JScrollPane scrollPane = new JScrollPane(mensagemEmail);
+			JOptionPane.showMessageDialog(null, scrollPane, "Digite a mensagem do e-mail:", JOptionPane.INFORMATION_MESSAGE);
+
+			// Criação da sessão
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+			try {
+				// Criação da mensagem
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress("arthur_benfica_appteste@hotmail.com"));
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(enderecoEmail));
+				message.setSubject(assuntoEmail);
+				message.setText(mensagemEmail.getText());
+
+				// Envio da mensagem
+				Transport.send(message);
+
+				JOptionPane.showMessageDialog(null, "E-mail enviado com sucesso!");
+
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
 			}
-		});
-
-		try {
-			// Criação da mensagem
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("arthur_benfica_appteste@hotmail.com"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("arthur_benfica@hotmail.com"));
-			message.setSubject("Testando envio de email pelo Hotmail com JavaMail");
-			message.setText("Olá, isso é um teste!");
-
-			// Envio da mensagem
-			Transport.send(message);
-
-			System.out.println("E-mail enviado com sucesso!");
-
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
+		} else {
+			JOptionPane.showMessageDialog(null, "Envio de e-mail cancelado!");
 		}
 	}
 }
